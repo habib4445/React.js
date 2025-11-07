282eade778218c78e2a594fead939840e40b6160
habib4445 <0124habibur@gmail.com>
2025-11-07 15:46:30 +0600

    add condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import ToDo from "./ToDo";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {/*  */}
       <h1>Vite + React</h1>
-      
- <Player name="Tamim" runs="5000"></Player>
-
-
+      <ToDo />
     </>
-  )
+  );
 }
 
-function Player(props){
-  return(
-    <div className='student'>
-      <h3>Name:{props.name}</h3>
-      <p>Runs:{props.runs}</p>
-    </div>
-  )
-}
-
-
-export default App
+export default App;
---
+++ src/Todo.jsx
@@ -0,0 +1,12 @@
+// // ToDo.jsx
+// export default function ToDo() {
+//   return (
+//     <li>Task:</li>
+//   )
+// }
+// conditional 
+export default function ToDo({ task, isDone, time = 0 }) {
+  return isDone
+    ? <li>Done: {task} time: {time}</li>
+    : <li>Not Done: {task}</li>;
+}
